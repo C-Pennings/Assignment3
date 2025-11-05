f694b2bed15e5fc8082a8bbd582ca1d6872a82bf
charl <charliepennings13@gmail.com>
2025-11-05 15:29:05 -0500

    Passed the invalid input tests

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(void) {
+	return 0;
+}
+
+const char* choices[] = {"rock", "paper", "scissors"};
 
 const char* RockPaperScissors(char p1[], char p2[]) {
 
-	return "Invalid";
+	//check for invalid inputs
+	bool isvalidP1 = false;
+	bool isvalidP2 = false;
+	for (int i = 0; i < 3; i++) {
+		if (strcmp(choices[i], p1) == 0) {
+			isvalidP1 = true;
+		}
+		if (strcmp(choices[i], p2) == 0) {
+			isvalidP2 = true;
+		}
+	}
+	if (isvalidP1 == false || isvalidP2 == false) { //if invalid, return error
+		return "Invalid";
+	}
+
+
+	return "Draw";
 }
